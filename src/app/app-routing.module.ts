463a36ed2e6fdc19ff32d21d267704e83dcbf644
wietze <wietze.wijma23@gmail.com>
2023-05-31 15:29:16 +0200

    routing toegevoegd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LidFormComponent} from "./lid-form/lid-form.component";
 import {TeamFormComponent} from "./team-form/team-form.component";
+import {LidListComponent} from "./lid-list/lid-list.component";
 
 const routes: Routes = [
   { path: 'lid', component: LidFormComponent },
+  { path: 'leden', component: LidListComponent },
   { path: 'team', component: TeamFormComponent }
 ];
 
